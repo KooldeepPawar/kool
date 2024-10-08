31ac41156883a3369e5c25bb4f02c007964bdded
Kuldeep Pawar <164771014+KooldeepPawar@users.noreply.github.com>
2024-10-08 07:23:46 +0530

    Create qrcode_generation.py

---
+++ qrcode_generation.py
@@ -0,0 +1,22 @@
+import qrcode  as qr
+import streamlit as st 
+
+
+def qrcodegeneration() :
+    
+    url = st.text_area("Enter Text to be converted","Enter URL for QR Code Generation")
+
+    submittbutton = st.button("Submit") 
+
+    if submittbutton :
+        img = qr.make(url)
+        # img.save("QR_Code.png")
+        st.balloons()
+        st.image("QR_Code.png")
+
+
+
+
+
+
+
